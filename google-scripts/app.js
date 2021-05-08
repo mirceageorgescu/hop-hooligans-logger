d9ec1822729396524dc29282d17611d206764cc7
Mircea Georgescu <mircea@192.168.0.113>
2021-05-09 01:03:06 +0300

    simplify things

--- google-scripts/app.js
+++ google-scripts/app.js
@@ -1,3 +1,7 @@
+function testDate() {
+  Logger.log(Utilities.formatDate(new Date(), "GMT+3", "MMM d, YYYY") + ' at ' + Utilities.formatDate(new Date(), "GMT+3", "hh:mm:ss"));
+}
+
 function doGet(e) { 
   Logger.log( JSON.stringify(e) );  // view parameters
   var result = 'Ok'; // assume success
@@ -9,26 +13,22 @@ function doGet(e) {
     var sheet = SpreadsheetApp.openById(sheet_id).getActiveSheet();   // get Active sheet
     var newRow = sheet.getLastRow() + 1;            
     var rowData = [];
-    rowData[0] = Utilities.formatDate(new Date(), "GMT+3", "MMM dd, YYYY") + ' at ' + Utilities.formatDate(new Date(), "GMT+3", "hh:mm:ss a"); // Timestamp in column A
+    rowData[0] = Utilities.formatDate(new Date(), "GMT+3", "MMM d, YYYY") + ' at ' + Utilities.formatDate(new Date(), "GMT+3", "hh:mm:ss"); //timestamp
     for (var param in e.parameter) {
       Logger.log('In for loop, param=' + param);
       var value = stripQuotes(e.parameter[param]);
       Logger.log(param + ':' + e.parameter[param]);
       switch (param) {
-        case 'sensor': //Parameter
+        case 'temperature':
           rowData[1] = value;
-          result += ', Written sensor';
-          break;
-        case 'temperature': //Parameter
-          rowData[2] = value; //Value in column B
           result += ', Written temperature';
           break;
-        case 'humidity': //Parameter
-          rowData[3] = value; //Value in column C
+        case 'humidity':
+          rowData[2] = value;
           result += ', Written humidity';
           break;
-        case 'battery': //Parameter
-          rowData[4] = value; //Value in column D
+        case 'battery':
+          rowData[3] = value;
           result += ', Written battery';
           break;  
         default:
